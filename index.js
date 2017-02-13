9cdc5e1e4cf614b10955b721951587e753527852
Antony Church <achurch44.ac@gmail.com>
2017-02-13 15:39:45 +0000

    Added file saving

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var nomnoml = require("nomnoml");
+var app = require('electron').remote; 
+var dialog = app.dialog;
+var fs = require('fs');
 
 var canvas = document.getElementById("target-canvas");
 var text = document.getElementById("text");
@@ -8,4 +11,21 @@ nomnoml.draw(canvas, text.value);
 
 var updateCanvas = function(){
     nomnoml.draw(canvas, text.value);
+};
+
+var saveFile = function(){
+    dialog.showSaveDialog(function (fileName) {
+       if (fileName === undefined){
+            console.log("You didn't save the file");
+            return;
+       }
+       // fileName is a string that contains the path and filename created in the save file dialog.  
+       fs.writeFile(fileName, text.value, function (err) {
+           if(err){
+               alert("An error ocurred creating the file "+ err.message)
+           }
+                        
+           alert("The file has been succesfully saved");
+       });
+    }); 
 };
