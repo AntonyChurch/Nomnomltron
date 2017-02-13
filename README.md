a7b7a81a22bbccebc4d5867e222b3c5549298650
Antony Church <achurch44.ac@gmail.com>
2017-02-13 15:28:46 +0000

    Added Readme and added textarea to update canvas

---
+++ README.md
@@ -0,0 +1,11 @@
+#Nomnomltron
+Nomnomltron is an application that brings the UML rendering technology of [nomnoml](http://nomnoml.io) to a standalone application using Electron.
+
+##Dependencies
+- NodeJS
+- Electron
+- nomnoml
+
+##Run instructions
+Navigate into the Nomnomltron directory and run:
+`electron .`
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var nomnoml = require("nomnoml");
 
 var canvas = document.getElementById("target-canvas");
+var text = document.getElementById("text");
+text.value = '[nomnoml] is -> [awesome]';
 
-nomnoml.draw(canvas, '[nomnoml] is -> [awesome]');
+nomnoml.draw(canvas, text.value);
+
+var updateCanvas = function(){
+    nomnoml.draw(canvas, text.value);
+};
